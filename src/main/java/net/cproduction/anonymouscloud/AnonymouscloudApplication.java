355cc0604c896d442b9a10337f9c48d3b614c630
gmu1026 <44140480+gmu1026@users.noreply.github.com>
2022-10-18 23:24:22 +0900

    Initial commit

---
+++ src/main/java/net/cproduction/anonymouscloud/AnonymouscloudApplication.java
@@ -0,0 +1,13 @@
+package net.cproduction.anonymouscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymouscloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymouscloudApplication.class, args);
+    }
+
+}
